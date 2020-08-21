08370cb944f8361d40ced0275c9b0446ad312c67
root <root@localhost.localdomain>
2020-08-21 08:50:25 -0400

    Updates

--- provisioning/bootstrap_kmaster_flannel.sh
+++ provisioning/bootstrap_kmaster_flannel.sh
@@ -3,17 +3,17 @@
 
 # Initialize Kubernetes
 echo "[TASK 1] Initialize Kubernetes Cluster"
-kubeadm init --apiserver-advertise-address=172.16.16.100 --pod-network-cidr=10.244.0.0/16 >> /root/kubeinit.log 2>/dev/null
+kubeadm init --pod-network-cidr=10.244.0.0/16 >> /root/kubeinit.log 2>/dev/null
 
 # Copy Kube admin config
-echo "[TASK 2] Copy kube admin config to Vagrant user .kube directory"
-mkdir /home/vagrant/.kube
-cp /etc/kubernetes/admin.conf /home/vagrant/.kube/config
-chown -R vagrant:vagrant /home/vagrant/.kube
+echo "[TASK 2] Copy kube admin config to brian user .kube directory"
+mkdir /home/brian/.kube
+cp /etc/kubernetes/admin.conf /home/brian/.kube/config
+chown -R brian:brian /home/brian/.kube
 
 # Deploy flannel network
 echo "[TASK 3] Deploy flannel network"
-su - vagrant -c "kubectl create -f /vagrant/kube-flannel.yaml"
+su - brian -c "kubectl create -f /brian/kube-flannel.yaml"
 
 # Generate Cluster join command
 echo "[TASK 4] Generate and save cluster join command to /joincluster.sh"
